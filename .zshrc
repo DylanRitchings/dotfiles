d9a603a015f5d588c010e67cfeb2a3c6bcc7fdee
mo-dylanritchings <dylan.ritchings@metoffice.gov.uk>
2025-01-29 12:49:27 +0000

    fix zshrc

--- .zshrc
+++ .zshrc
@@ -1,2 +1,22 @@
+export ZSH="$HOME/.oh-my-zsh"
+export PATH=/Users/dylan/Library/Python/3.12/bin:$PATH
 
+ZSH_THEME="eastwood"
+
+plugins=(
+    fzf
+    fzf-tab
+    zsh-autosuggestions
+    git
+    colorize
+    history
+    aws
+    web-search
+    zsh-syntax-highlighting
+    F-Sy-H
+    #emacs
+)
+
+source $ZSH/oh-my-zsh.sh
+export EDITOR=nvim
 [ -f "~/dev/personal_repos/dotfiles/bash/init.sh" ] && source ~/dev/personal_repos/dotfiles/bash/init.sh
--- bash/setup.sh
+++ bash/setup.sh
@@ -1,25 +1,4 @@
 # Path to your oh-my-zsh installation.
-export ZSH="$HOME/.oh-my-zsh"
-export PATH=/Users/dylan/Library/Python/3.12/bin:$PATH
-
-ZSH_THEME="eastwood"
-
-plugins=(
-    fzf
-    fzf-tab
-    zsh-autosuggestions
-    git
-    colorize
-    history
-    aws
-    web-search
-    zsh-syntax-highlighting
-    F-Sy-H
-    #emacs
-)
-
-source $ZSH/oh-my-zsh.sh
-export EDITOR=nvim
 # ------------ZSH fzf-tab ------------
 # set descriptions format to enable group support
 zstyle ':completion:*:descriptions' format '[%d]'
